c656045243e525f4c6cd38ced9989bb450055475
Brent Yurek <brentagainagain@gmail.com>
2019-10-11 17:28:42 -0500

    Done!

--- Assets/activateMushroom.cs
+++ Assets/activateMushroom.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class lightToggle : MonoBehaviour
+public class activateMushroom : MonoBehaviour
 {
-    public GameObject light;
-
+    public AudioSource sound;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -20,8 +19,6 @@ public class lightToggle : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collision with " + other.gameObject.name);
-        if (other.gameObject.name.Contains("HTC_Vive_controller"))
-            light.SetActive(!light.activeSelf);
+        sound.Play();
     }
 }
---
+++ Assets/objToggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objToggle : MonoBehaviour
+{
+    public GameObject obj;
+    public bool startActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj.SetActive(startActive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision with " + other.gameObject.name);
+        if (other.gameObject.name.Contains("HTC_Vive_controller"))
+            obj.SetActive(!obj.activeSelf);
+    }
+}
