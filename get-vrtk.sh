c4f0a00a72ca107e03956e6642f50dad3fc555d3
Brent Yurek <brentagainagain@gmail.com>
2019-10-15 12:19:47 -0500

    get-vrtk.sh now rolls back to a specific commit for VRTK because the current state of it is borked

--- get-vrtk.sh
+++ get-vrtk.sh
@@ -2,4 +2,5 @@ cd Assets
 rm -rf VRTK
 git clone --recurse-submodules https://github.com/ExtendRealityLtd/VRTK.git
 cd VRTK
+git reset --hard 29d098ce511e9c18a189327b74029b193d6011a9
 git submodule init && git submodule update
